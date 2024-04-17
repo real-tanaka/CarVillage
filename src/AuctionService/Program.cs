afa7bd2ef68bedf9edcebfb92f61157347cbe8d3
Tanaka Shoko <realtanakashoko@gmail.com>
2024-04-17 19:29:18 +0200

    End of Section 2

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Immutable;
+using AuctionService.Data;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
